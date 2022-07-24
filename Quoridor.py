9eab4da5aa0f35c800c05507e2861ac9c56d9ca5
wbmcglaughlin <wmcg0001@student.monash.edu>
2022-07-24 22:34:47 +1000

    feature(Quoridor): tiles can now be placed

--- Quoridor.py
+++ Quoridor.py
@@ -3,21 +3,137 @@ from typing import List
 
 
 class QuoridorGame:
-    def __init__(self, side_squares: int, total_tiles: int, players: int):
+    def __init__(self,
+                 side_squares: int,
+                 total_tiles: int,
+                 players: int,
+                 position: Vector2,
+                 width: int,
+                 border: int):
+
         self.side_squares = side_squares
         self.tile_count = total_tiles
 
+        self.position = position
+        self.width = width
+        self.border = border
+        self.square_width = (self.width - self.border * (self.side_squares + 1)) / self.side_squares
+
+        self.player_pos: List[int] = []
+        self.player_col: List[Color] = []
         self.player_tiles: List[int] = []
-        self.board_tiles = []
+        self.board_tiles: List[Tile] = []
 
-    def draw_board(self, position: Vector2, width: int, border: float):
-        draw_rectangle(position.x, position.y, width, width, GOLD)
-        square_width = (width - border * (self.side_squares  + 1)) / self.side_squares
+        self.board_squares = self.get_board_squares()
+        self.tile_squares = self.get_tile_squares()
 
-        for i in range(self.side_squares):
-            for j in range(self.side_squares):
-                draw_rectangle(position.x + (i * (square_width + border)) + border,
-                               position.y + (j * (square_width + border)) + border,
-                               square_width,
-                               square_width,
-                               BLACK)
+        self.set_player_pos(players)
+
+    def draw(self):
+        draw_rectangle(self.position.x, self.position.y, self.width, self.width, GOLD)
+
+        for rec in self.board_squares:
+            draw_rectangle_rec(rec, LIGHTGRAY)
+
+        for rec in self.tile_squares:
+            draw_rectangle_rec(rec, BROWN)
+
+        self.draw_players()
+
+        self.draw_tiles()
+
+    def place_tile(self, rec_index, orientation):
+        self.board_tiles.append(Tile(rec_index, orientation))
+
+    def draw_players(self):
+        for player_ind, player_pos in enumerate(self.player_pos):
+            draw_circle(self.board_squares[player_pos].x + self.square_width / 2,
+                        self.board_squares[player_pos].y + self.square_width / 2,
+                        self.square_width / 4,
+                        self.player_col[player_ind])
+
+    def draw_tiles(self):
+        for tile in self.board_tiles:
+            if tile.orientation == 0:
+                draw_rectangle(
+                    self.tile_squares[tile.rec_index].x,
+                    self.tile_squares[tile.rec_index].y - self.square_width,
+                    self.border,
+                    int(self.square_width * 2 + self.border),
+                    PURPLE
+                )
+            else:
+                draw_rectangle(
+                    self.tile_squares[tile.rec_index].x - self.square_width,
+                    self.tile_squares[tile.rec_index].y,
+                    int(self.square_width * 2 + self.border),
+                    self.border,
+                    PURPLE
+                )
+
+    def set_player_pos(self, players_count):
+        pos = []
+        col = []
+
+        if players_count != 2 or players_count != 4:
+            print("Players Count not 2 or 4, setting to 2")
+            players_count = 2
+
+        pos.append(int(self.side_squares / 2))
+        pos.append(self.side_squares * self.side_squares - int(self.side_squares / 2) - 1)
+        col.append(BLUE)
+        col.append(DARKBLUE)
+
+        if players_count == 4:
+            pos.append(self.side_squares * 5)
+            pos.append(self.side_squares * 4 + 1)
+
+        self.player_pos = pos
+        self.player_col = col
+
+    def get_board_squares(self):
+        board_squares = []
+
+        for j in range(self.side_squares):
+            for i in range(self.side_squares):
+                rec = Rectangle(self.position.x + (i * (self.square_width + self.border)) + self.border,
+                                self.position.y + (j * (self.square_width + self.border)) + self.border,
+                                self.square_width,
+                                self.square_width)
+
+                board_squares.append(rec)
+
+        return board_squares
+
+    def get_tile_squares(self):
+        recs = []
+
+        square_width = (self.width - self.border * (self.side_squares + 1)) / self.side_squares
+
+        for j in range(self.side_squares + 1):
+            for i in range(self.side_squares + 1):
+                recs.append(Rectangle(
+                    self.position.x + (i * (square_width + self.border)),
+                    self.position.y + (j * (square_width + self.border)),
+                    self.border,
+                    self.border
+                ))
+
+        recs.pop(-1)
+        recs.pop(-self.side_squares)
+        recs.pop(self.side_squares)
+        recs.pop(0)
+
+        return recs
+
+    def is_legal_tile_square(self, rec_index):
+        for tile in self.board_tiles:
+            if tile.rec_index == rec_index:
+                return False
+
+        return True
+
+class Tile:
+    def __init__(self, rec_index, orientation):
+        self.rec_index = rec_index
+        self.orientation = orientation
--- main.py
+++ main.py
@@ -2,31 +2,44 @@ from raylibpy import *
 from typing import List
 from Quoridor import QuoridorGame
 
+
 def main():
     width = 800
     height = 800
 
     border = 100
     top_left_corner = Vector2(border, border)
+    square_border = 15
 
     init_window(width, height, "quoridor bot")
 
-    quoridor_game = QuoridorGame(9, 20, 2)
+    quoridor_game = QuoridorGame(9, 20, 2, top_left_corner, width - 2 * border, square_border)
 
     mouse_point: Vector2
 
     set_target_fps(60)
 
-    square_border = 30
+    current_orientation = 0
 
     while not window_should_close():
 
         mouse_point = get_mouse_position()
 
+        if is_mouse_button_pressed(MOUSE_LEFT_BUTTON):
+            for rec_ind, rec in enumerate(quoridor_game.tile_squares):
+                if check_collision_point_rec(mouse_point, rec):
+                    if quoridor_game.is_legal_tile_square(rec_ind):
+                        quoridor_game.place_tile(rec_ind, current_orientation)
+
+        if is_mouse_button_pressed(MOUSE_RIGHT_BUTTON):
+            current_orientation += 1
+            current_orientation %= 2
+
         begin_drawing()
         clear_background(RAYWHITE)
 
-        quoridor_game.draw_board(top_left_corner, width - 2 * border, square_border)
+        quoridor_game.draw()
+
         end_drawing()
 
     close_window()
